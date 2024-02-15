4f0a442a3a9436674359e8ef48c6ac48ebe3f49c
Royal <royalhuseyn902@gmail.com>
2024-02-15 16:52:30 +0400

    Implement the buildTree(arr) method

--- tree.js
+++ tree.js
@@ -1,3 +1,33 @@
 import Node from './node.js';
 
-class Tree {}
+class Tree {
+  constructor(arr) {
+    this.root = Tree.buildTree(arr);
+  }
+
+  static buildTree(arr) {
+    const sorted = [...new Set(arr)].sort((a, b) => a - b);
+
+    if (sorted.length === 0) return null;
+
+    const mid = Math.floor((sorted.length - 1) / 2);
+    const root = new Node(sorted[mid]);
+    root.left = Tree.buildTree(sorted.slice(0, mid));
+    root.right = Tree.buildTree(sorted.slice(mid + 1));
+
+    return root;
+  }
+}
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
